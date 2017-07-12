8621118fb92ca167f959e37657731afa17f207a3
Bastian Bukatz <bastian@bastians-macbook-air.home>
2017-07-12 20:38:54 +0200

    Initial Commit

---
+++ SoudtouchPresets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoudtouchPresets
+//
+//  Created by Bastian Bukatz on 12.07.17.
+//  Copyright © 2017 Bastian Bukatz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
